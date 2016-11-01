2d474be8a3d873b8d4ff8947c36a8b42e0406757
Fernando Callata <fernandohre@hotmail.com>
2016-11-01 18:31:55 -0200

    Funcionamento inicial do programa, apenas na IDE, não foi gerado o jar

--- Calcular/src/main/java/com/github/fernandohre/calcular/Principal.java
+++ Calcular/src/main/java/com/github/fernandohre/calcular/Principal.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String calculo = input.nextLine();
-        System.out.println(Calcular.exprPara(calculo));
+        String expressao;
+        expressao = input.nextLine();
+        try {
+            System.out.println(Calcular.exprPara(expressao));
+            System.exit(0);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Expressão não fornecida");
+            System.exit(1);
+        } catch (Exception e) {
+            System.out.println("Expressão inserida inválida");
+            System.exit(1);
+        }
     }
 }
