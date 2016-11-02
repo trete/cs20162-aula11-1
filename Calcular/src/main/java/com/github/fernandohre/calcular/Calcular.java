56afe32a9f1ffb45d3b48cf5f669d85a9c8dcb2d
fernando callata <fernando.henrique@geopix.com.br>
2016-11-02 19:00:59 -0200

    Apenas refatorando o nome da classe para verificar o funcionamento do jar

--- Calcular/src/main/java/com/github/fernandohre/calcular/Calcular.java
+++ Calcular/src/main/java/com/github/fernandohre/calcular/Calcular.java
@@ -1,24 +1,20 @@
+
 package com.github.fernandohre.calcular;
-import com.github.kyriosdata.parser.*;
-import java.util.List;
-/**
- *  Classe que contém método que realizam as operações aritméticas sugeridas
- *  pelo usuário.
- * 
- */
+import java.util.Scanner;
+
 public class Calcular {
-    /**
-     *  Método que recebe argumentos sugeridos pelo usuário para o cáculo
-     *  aritmético, fazendo o usuo da classe Lexer para tratar a expressão
-     *  e o parsers.
-     * 
-     * @param expressao expressão matemática sugerida pelo usuário, ex: 5 + 2,
-     * 2 * (5 + 10), 5 * (5*8) e etc;
-     * @return o resultado da expressão aritmética.
-     */
-    public static float exprPara(String expressao) {
-        List<Token> tokens = new Lexer(expressao).tokenize();
-        Parser parser = new Parser(tokens);
-        return parser.expressao().valor();
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(System.in);
+            String expressao = input.nextLine();
+            System.out.println(ImplementacaoCalcular.exprPara(expressao));
+            System.exit(0);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Expressão não fornecida");
+            System.exit(1);
+        } catch (Exception e) {
+            System.out.println("Expressão inserida inválida");
+            System.exit(1);
+        }
     }
 }
---
+++ Calcular/src/main/java/com/github/fernandohre/calcular/ImplementacaoCalcular.java
@@ -0,0 +1,24 @@
+package com.github.fernandohre.calcular;
+import com.github.kyriosdata.parser.*;
+import java.util.List;
+/**
+ *  Classe que contém método que realizam as operações aritméticas sugeridas
+ *  pelo usuário.
+ * 
+ */
+public class ImplementacaoCalcular {
+    /**
+     *  Método que recebe argumentos sugeridos pelo usuário para o cáculo
+     *  aritmético, fazendo o usuo da classe Lexer para tratar a expressão
+     *  e o parsers.
+     * 
+     * @param expressao expressão matemática sugerida pelo usuário, ex: 5 + 2,
+     * 2 * (5 + 10), 5 * (5*8) e etc;
+     * @return o resultado da expressão aritmética.
+     */
+    public static float exprPara(String expressao) {
+        List<Token> tokens = new Lexer(expressao).tokenize();
+        Parser parser = new Parser(tokens);
+        return parser.expressao().valor();
+    }
+}
