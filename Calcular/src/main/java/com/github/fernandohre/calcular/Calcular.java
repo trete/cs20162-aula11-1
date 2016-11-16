e0b9c6d538db3a48bb52e5c90d69ef902505e830
thiagoberaldo1 <thiago._.b.b@hotmail.com>
2016-11-16 19:30:28 -0200

    removendo tratamento desnecessario

--- Calcular/src/main/java/com/github/fernandohre/calcular/Calcular.java
+++ Calcular/src/main/java/com/github/fernandohre/calcular/Calcular.java
@@ -9,9 +9,6 @@ public class Calcular {
             String expressao = input.nextLine();
             System.out.println(ImplementacaoCalcular.exprPara(expressao));
             System.exit(0);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Expressão não fornecida");
-            System.exit(1);
         } catch (Exception e) {
             System.out.println("Expressão inserida inválida");
             System.exit(1);
